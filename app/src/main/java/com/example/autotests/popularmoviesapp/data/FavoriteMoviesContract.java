52f7e4127ccd8c3fd1d7586df0152d4fef02879f
KARINA BERNICE/LGESP RKarina.berniceD MC SW(karina.bernice@lge.com) <karina.bernice@lge.com>
2017-12-11 13:23:31 -0200

    Added support for querying and adding new Favorites 2.

--- app/src/main/java/com/example/autotests/popularmoviesapp/data/FavoriteMoviesContract.java
+++ app/src/main/java/com/example/autotests/popularmoviesapp/data/FavoriteMoviesContract.java
@@ -11,5 +11,8 @@ public class FavoriteMoviesContract  {
         public static final String TABLE_NAME = "favorites";
         public static final String COLUMN_NAME = "name";
         public static final String COLUMN_DATE = "date";
+        public static final String COLUMN_OVERVIEW = "overview";
+        public static final String COLUMN_RATING = "rating";
+        public static final String COLUMN_POSTER = "poster";
     }
 }
